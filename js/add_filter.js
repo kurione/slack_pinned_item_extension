fcee5f02095beb65cd3695d2e7c6fa0949c002d0
kurione <mohaya.stick@gmail.com>
2015-11-27 14:44:25 +0900

    fix

--- js/add_filter.js
+++ js/add_filter.js
@@ -6,12 +6,12 @@ get_exttypes = function() {
       arr.push(ext);
     }
   });
-  console.log(arr);
+  //console.log(arr);
   return arr;
 }
 
 create_selectbox = function(arr) {
-  $(".section_content.pinned_items").prepend("<select name='pinned_items_filter'></select>");
+  $(".section_content.pinned_items").prepend("<label for='pinned_items_filter'>filter by:</label><select name='pinned_items_filter' class='form-control'></select>");
   $("select[name='pinned_items_filter']")
     .append("<option value='ALL'>ALL</option>")
     .append("<option value='message'>message</option>");
@@ -49,6 +49,7 @@ show_selected_ext = function(selected_ext){
 
 add_filter = function(){
   if ($("select[name='pinned_items_filter']").length > 0) return;
+  if ($("div.section_content.pinned_items div.pinned_item").length == 0) return;
   create_selectbox(get_exttypes());
   $("select[name='pinned_items_filter']").change(function() {
     var selected_ext = $(this).val();
@@ -62,20 +63,18 @@ add_filter = function(){
   });
 }
 
-wait_while_loading = function(){
-  if ($("div.section_content.pinned_items div.pinned_item").length == 0){
-    setTimeout(wait_while_loading, 500);
-    console.log('wait:500ms');
-  } else {
+wait_while_page_pinned_items = function(){
+  if ($("div.channel_page_pinned_items div.section_content").length > 0){
     add_filter();
   }
+  setTimeout(wait_while_page_pinned_items, 300);
 }
 
 start = function(){
   $(document).on("click", "div.channel_page_pinned_items div.section_header", function() {
     add_filter();
   });
-  wait_while_loading();
+  wait_while_page_pinned_items();
 }
 
 start();
