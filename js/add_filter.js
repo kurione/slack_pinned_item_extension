36382a82c0634f3f837063ab1045285577338e48
kurione <mohaya.stick@gmail.com>
2015-11-27 11:06:05 +0900

    wip

--- js/add_filter.js
+++ js/add_filter.js
@@ -1,58 +1,66 @@
-$.wait = function(msec) {
-  var d = new $.Deferred;
-  setTimeout(function(){
-    d.resolve(msec);
-  }, msec);
-  return d.promise();
-};
-
-$.wait(8000).done(function() {
-
-//$("div.section_header.no_bottom_margin.display_flex.align_items_center a:contains('Pinned Items')").on('click', function(){
-
+get_exttypes = function() {
   var arr = [];
-  $(".section_content.pinned_items p.pin_metadata").each(function(){
+  $(".section_content.pinned_items p.pin_metadata").each(function() {
     var ext = $(this).text().trim();
-    if ($.inArray(ext, arr) == -1){
+    if ($.inArray(ext, arr) == -1) {
       arr.push(ext);
     }
   });
   console.log(arr);
+  return arr;
+}
 
+create_selectbox = function(arr) {
   $(".section_content.pinned_items").prepend("<select name='pinned_items_filter'></select>");
-  $("select[name='pinned_items_filter']").append("<option value='ALL'>ALL</option>");
-  $("select[name='pinned_items_filter']").append("<option value='message'>message</option>");
-  $.each(arr, function(){
+  $("select[name='pinned_items_filter']")
+    .append("<option value='ALL'>ALL</option>")
+    .append("<option value='message'>message</option>");
+  $.each(arr, function() {
     $("select[name='pinned_items_filter']").append("<option value='" + this + "'>" + this + "</option>");
   });
+}
 
-  $("select[name='pinned_items_filter']").change(function(){
+add_filter = function(){
+  if ($("select[name='pinned_items_filter']").length > 0) return;
+  create_selectbox(get_exttypes());
+  $("select[name='pinned_items_filter']").change(function() {
     var selected_ext = $(this).val();
-    if (selected_ext == "ALL"){
-      $("div.section_content.pinned_items div.pinned_item").each(function(){
+    if (selected_ext == "ALL") {
+      $("div.section_content.pinned_items div.pinned_item").each(function() {
         $(this).show();
       });
-    } else if (selected_ext == "message"){
-      $("div.section_content.pinned_items div.pinned_item").each(function(){
+    } else if (selected_ext == "message") {
+      $("div.section_content.pinned_items div.pinned_item").each(function() {
         $(this).show();
       });
-      $("div.section_content.pinned_items p.pin_metadata").each(function(){
+      $("div.section_content.pinned_items p.pin_metadata").each(function() {
         $(this).parent('div').hide();
       });
     } else {
-      $("div.section_content.pinned_items div.pinned_item").each(function(){
+      $("div.section_content.pinned_items div.pinned_item").each(function() {
         $(this).hide();
       });
-      $("div.section_content.pinned_items p.pin_metadata").each(function(){
+      $("div.section_content.pinned_items p.pin_metadata").each(function() {
         var ext = $(this).text().trim();
-        if (ext == selected_ext){
+        if (ext == selected_ext) {
           $(this).parent('div').show();
         }
       });
     }
   });
+}
+
+$(document).on("click", "div.channel_page_pinned_items div.section_header", function() {
+  add_filter();
 });
 
-get_pinned_items = function(){
-
+wait_while_loading = function(){
+  if ($("div.section_content.pinned_items div.pinned_item").length == 0){
+    setTimeout(wait_while_loading, 500);
+    console.log('wait:500ms');
+  } else {
+    add_filter();
+  }
 }
+
+wait_while_loading();
